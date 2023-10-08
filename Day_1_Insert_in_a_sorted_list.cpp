5310b032df263051d59b93b51dd716598a4700e5
its-aky <kumaramityadav230@gmail.com>
2023-10-08 11:33:09 +0530

    Initial Commit Renamed the file

---
+++ Day_1_Insert_in_a_sorted_list.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+// } Driver Code Ends
+/*
+structure of the node of the list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+    // Should return head of the modified linked list
+    Node *sortedInsert(struct Node* head, int data) {
+        Node *start=head,*back;
+        bool flag=false;
+        while(head!=NULL){
+            if(head->data>=data){
+                if(head==start)flag=true;
+                break;
+            }
+            back=head;
+            head=head->next;
+        }
+        
+        Node *temp=new Node(data);
+        //temp->data=data;
+        //temp->next=NULL;
+        if(flag){
+            temp->next=head;
+            start=temp;
+        }
+        else{
+            back->next=temp;
+            temp->next=head;
+        }
+        
+        
+        
+        return start;
+        
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+        
+		int data;
+		cin>>data;
+		
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		
+		int k;
+		cin>>k;
+		Solution obj;
+		head = obj.sortedInsert(head,k);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
